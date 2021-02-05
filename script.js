c9495d7e75eb292b3dc2282d3388cabb12893f41
umaumax <umauma7083@gmail.com>
2021-02-05 23:35:06 +0900

    Fix url meaning
      the link includes commit itself

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ $(function() {
         console.log('This is github pull request page');
 
         var commit_hash_hrefs = $('div.js-commit-group-commits .js-commit .text-right.ml-1 a');
-        commit_hash_hrefs.each(function(i, e) {
-            var url = e.href;
-            var diff_url = url.replace(/\/commits\//, '/files/') + '..HEAD'
-            $(e).parent().append('<a href="' + diff_url + '"class="link-gray">' + '🎯DIFF' + '</a>');
-        });
+        for (var i = 0; i < commit_hash_hrefs.length; i++) {
+            var elem = commit_hash_hrefs[i];
+            var prev_elem = null;
+            var diff_url = null;
+            if (i == 0) {
+                var url = elem.href;
+                diff_url = url.replace(/\/commits\/.*/, '/files/')
+            } else {
+                prev_elem = commit_hash_hrefs[i - 1];
+                var url = prev_elem.href;
+                diff_url = url.replace(/\/commits\//, '/files/') + '..HEAD'
+            }
+            $(elem).parent().append('<a href="' + diff_url + '"class="link-gray">' + '🎯DIFF' + '</a>');
+        }
     }
 });
